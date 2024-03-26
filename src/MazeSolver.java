e8b64d06ea2fb48e65da57b067874cad7935dab7
steviekh <stevie.halprin@menloschool.org>
2024-03-26 12:15:19 -0700

    Sorry for the late commit, I completed parts 1 and 2 last Thursday, then finished the BFS and DFS methods on Friday. I commented the rest of the code today.

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -5,7 +5,12 @@
  */
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
+// Stevie K. Halprin
+// 22/3/2024
 public class MazeSolver {
     private Maze maze;
 
@@ -29,7 +34,31 @@ public class MazeSolver {
     public ArrayList<MazeCell> getSolution() {
         // TODO: Get the solution from the maze
         // Should be from start to end cells
-        return null;
+
+        // ArrayList that holds the cells that are part of the solution to the maze
+        ArrayList<MazeCell> solCells = new ArrayList<>();
+        // Stack that holds the parent cells to every solution cell
+        Stack<MazeCell> parCells = new Stack<MazeCell>();
+        // Makes the cell that will be started at the end cell of the maze
+        MazeCell current = maze.getEndCell();
+
+        // While the given cell isn't the start of the maze...
+        while (current != maze.getStartCell()) {
+            // Add the current cell to the Stack of parent cells
+            parCells.push(current);
+            // Make the new current cell the parent of the current cell
+            current = current.getParent();
+        }
+        // Finally, add the start cell to the Stack of parent cells
+        parCells.push(maze.getStartCell());
+
+        // While there are still cells in parCells, add the top element to ArrayList solCells
+        while (!parCells.empty()) {
+            solCells.add(parCells.pop());
+        }
+
+        // Return the newly filled ArrayList of solution cells
+        return solCells;
     }
 
     /**
@@ -39,7 +68,49 @@ public class MazeSolver {
     public ArrayList<MazeCell> solveMazeDFS() {
         // TODO: Use DFS to solve the maze
         // Explore the cells in the order: NORTH, EAST, SOUTH, WEST
-        return null;
+
+        // Makes the cell currently being explored the starting cell
+        MazeCell current = maze.getStartCell();
+        // Creates new stack to which the cells to be explored are added
+        Stack<MazeCell> path = new Stack<>();
+        // Adds the start cell to the solution
+        path.push(maze.getStartCell());
+
+        // While the cell currently being explored isn't the end cell, continue
+        while (current != maze.getEndCell()) {
+            // Set the current cell to the top element in path, then remove that element
+            current = path.pop();
+            // If cell to the North of current cell can be explored, add it to path
+            // Also set the parent of that cell to the current cell, and make the cell explored
+            if (maze.isValidCell(current.getRow() - 1, current.getCol())) {
+                maze.getCell(current.getRow() - 1, current.getCol()).setParent(current);
+                maze.getCell(current.getRow() - 1, current.getCol()).setExplored(true);
+                path.push(maze.getCell(current.getRow() - 1, current.getCol()));
+            }
+            // If cell to the East of current cell can be explored, add it to path
+            // Also set the parent of that cell to the current cell, and make the cell explored
+            if (maze.isValidCell(current.getRow(), current.getCol() + 1)) {
+                maze.getCell(current.getRow(), current.getCol() + 1).setParent(current);
+                maze.getCell(current.getRow(), current.getCol() + 1).setExplored(true);
+                path.push(maze.getCell(current.getRow(), current.getCol() + 1));
+            }
+            // If cell to the South of current cell can be explored, add it to path
+            // Also set the parent of that cell to the current cell, and make the cell explored
+            if (maze.isValidCell(current.getRow() + 1, current.getCol())) {
+                maze.getCell(current.getRow() + 1, current.getCol()).setParent(current);
+                maze.getCell(current.getRow() + 1, current.getCol()).setExplored(true);
+                path.push(maze.getCell(current.getRow() + 1, current.getCol()));
+            }
+            // If cell to the West of current cell can be explored, add it to path
+            // Also set the parent of that cell to the current cell, and make the cell explored
+            if (maze.isValidCell(current.getRow(), current.getCol() - 1)) {
+                maze.getCell(current.getRow(), current.getCol() - 1).setParent(current);
+                maze.getCell(current.getRow(), current.getCol() - 1).setExplored(true);
+                path.push(maze.getCell(current.getRow(), current.getCol() - 1));
+            }
+        }
+        // Return ArrayList of solution by running getSolution()
+        return getSolution();
     }
 
     /**
@@ -49,7 +120,48 @@ public class MazeSolver {
     public ArrayList<MazeCell> solveMazeBFS() {
         // TODO: Use BFS to solve the maze
         // Explore the cells in the order: NORTH, EAST, SOUTH, WEST
-        return null;
+
+        // Sets the current cell to the starting cell of the maze
+        MazeCell current = maze.getStartCell();
+        // Creates new Queue to which the cells to be explored are added
+        Queue<MazeCell> path = new LinkedList<>();
+        // Adds the start cell to the solution
+        path.add(maze.getStartCell());
+
+        while (current != maze.getEndCell()) {
+            // Set the current cell to the next element in path, then remove that element
+            current = path.remove();
+            // If cell to the North of current cell can be explored, add it to path
+            // Also set the parent of that cell to the current cell, and make the cell explored
+            if (maze.isValidCell(current.getRow() - 1, current.getCol())) {
+                maze.getCell(current.getRow() - 1, current.getCol()).setParent(current);
+                maze.getCell(current.getRow() - 1, current.getCol()).setExplored(true);
+                path.add(maze.getCell(current.getRow() - 1, current.getCol()));
+            }
+            // If cell to the East of current cell can be explored, add it to path
+            // Also set the parent of that cell to the current cell, and make the cell explored
+            if (maze.isValidCell(current.getRow(), current.getCol() + 1)) {
+                maze.getCell(current.getRow(), current.getCol() + 1).setParent(current);
+                maze.getCell(current.getRow(), current.getCol() + 1).setExplored(true);
+                path.add(maze.getCell(current.getRow(), current.getCol() + 1));
+            }
+            // If cell to the South of current cell can be explored, add it to path
+            // Also set the parent of that cell to the current cell, and make the cell explored
+            if (maze.isValidCell(current.getRow() + 1, current.getCol())) {
+                maze.getCell(current.getRow() + 1, current.getCol()).setParent(current);
+                maze.getCell(current.getRow() + 1, current.getCol()).setExplored(true);
+                path.add(maze.getCell(current.getRow() + 1, current.getCol()));
+            }
+            // If cell to the West of current cell can be explored, add it to path
+            // Also set the parent of that cell to the current cell, and make the cell explored
+            if (maze.isValidCell(current.getRow(), current.getCol() - 1)) {
+                maze.getCell(current.getRow(), current.getCol() - 1).setParent(current);
+                maze.getCell(current.getRow(), current.getCol() - 1).setExplored(true);
+                path.add(maze.getCell(current.getRow(), current.getCol() - 1));
+            }
+        }
+        // Return ArrayList of solution by running getSolution()
+        return getSolution();
     }
 
     public static void main(String[] args) {
